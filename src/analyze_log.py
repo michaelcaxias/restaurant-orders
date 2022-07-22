0c9c4d71fff8d835edaf17869f9cd31197cf61e7
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-07-21 21:48:02 -0300

    :sparkles: feat: inicia implementação de leitura e hashmap

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,9 @@
-def maria_favorites_place():
-    raise NotImplementedError
+import csv
+
+def maria_favorites_plates(orders_list):
+    all_marias_plates = {}
+
+    return all_marias_plates
 
 def many_times_arnaldo_asked():
     raise NotImplementedError
@@ -11,4 +15,14 @@ def which_days_did_joao_never_go_to_the_cafeteria():
     raise NotImplementedError
 
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file.endswith('csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    try:
+        with open(path_to_file) as file:
+            file_reader = csv.reader(file, delimiter=",", quotechar='"')
+            orders_list = [order for order in file_reader]
+            maria_favorites = maria_favorites_plates(orders_list)
+            return maria_favorites
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
