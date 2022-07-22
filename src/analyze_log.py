f4f6bac13f8607a0a29e2518c421b2ad16213254
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-07-21 21:15:14 -0300

    :sparkles: feat: inicia funções de cada pergunta

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,14 @@
+def maria_favorites_place():
+    raise NotImplementedError
+
+def many_times_arnaldo_asked():
+    raise NotImplementedError
+
+def which_plates_joao_never_asked():
+    raise NotImplementedError
+
+def which_days_did_joao_never_go_to_the_cafeteria():
+    raise NotImplementedError
+
 def analyze_log(path_to_file):
     raise NotImplementedError
